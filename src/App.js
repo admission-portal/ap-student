1cab7db6d78d0c2ebf12228d9455932641c779e4
Tejas Ladhani <67834407+Tejas-Ladhani@users.noreply.github.com>
2021-09-28 22:57:25 +0530

    integration | sessionStorage | jwt decode | usermail

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import {
 } from 'react-router-dom';
 
 import Main from './main';
+import Intermediate from './intermediate';
 import {Landing} from './pages/common/';
 function App() {
 
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/s' component={Main}/>
-        <Route path='/' component={Landing}/>
+        <Route path='/s' component={Main} exact/>
+        <Route path='/' component={Landing} exact/>
+        <Route path='/interm' component={Intermediate} exact/> 
       </Switch>
     </BrowserRouter>
   );
---
+++ src/intermediate.js
@@ -0,0 +1,35 @@
+import jwt from 'jwt-decode'
+import { useHistory } from 'react-router-dom';
+export default function Itermediate() {
+    const history = useHistory();
+
+    try {
+        /*  NOTE: need to check the condition here
+        *   try-catch is temp here
+        */
+        let temp = window.location.href.split('#')[1];
+        let paramString = temp.split('&')[0];
+        let queryString = new URLSearchParams(paramString);
+        let id_token;
+        for (let pair of queryString.entries()) {
+            if (pair[0] === 'id_token') {
+                id_token = pair[1];
+            }
+        }
+        if (id_token != null) {
+            sessionStorage.setItem('id_token', id_token);
+            sessionStorage.setItem('u_decoded', JSON.stringify(jwt(id_token)))
+            history.push('/s/Profile/edit')
+    
+        }
+    }
+    catch (e) {
+        console.log(e)
+    }
+    
+    return (
+        <div>
+            Redirecting...
+        </div>
+    )
+}
